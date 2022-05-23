866c6b6cf7d6480bed63778ad2d0fe6e9198240c
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-05-23 12:26:14 +0100

    Add setup.py to configure for pre-commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pre-commit-puml",
+    version="0.0.1",
+    description="A PlantUML pre-commit hook that generates diagrams for use in technical documentation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/acodeninja/pre-commit-puml",
+    project_urls={
+        "Bug Tracker": "https://github.com/acodeninja/pre-commit-puml/issues",
+    },
+    python_requires=">=3.6",
+)
